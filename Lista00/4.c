f6d25b265eb84cc3e36e71024fdee78ef37a095d
Joel <JoelVIctor1746@gmail.com>
2019-02-13 12:05:20 -0200

    Update 4.c

--- Lista00/4.c
+++ Lista00/4.c
@@ -1,63 +1,64 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int ehPrimo(int numero);
+bool ehPrimo(int numero);
 
 int main() {
 	int numero;
 	int copia;
+	int i;
+	int key = false;
 
-	INICIO:
-	printf("Digite um numero primo!\n");
-	scanf("%d", &numero);
-
-	if (ehPrimo(numero) == 1) {
-		printf("ele eh primo\n");
-
-		copia = numero;
-		for (int i = 0; i < 2;) {
-			copia--;
-			if (copia == 1) {
-				printf("nao existe mais numeros a esquerda de %d\n", numero);
-				break;
-			}else if (ehPrimo(copia) == 1) {
-				i++;
-				printf("O %dº numero primo a esquerda de %d eh %d\n", i, numero, copia);
-			}
-			
-		}
-
-		copia = numero;
-		for (int i = 0; i < 2;) {
-			copia++;
 	
-			if (ehPrimo(copia) == 1) {
-				i++;
-				printf("O %dº numero primo a direita de %d eh %d\n", i, numero, copia);
+	while(key == false){
+		printf("Digite um numero primo!\n");
+		scanf("%d", &numero);
+
+		if(ehPrimo(numero) == true){
+			key = true;	
+			printf("ele eh primo\n");
+	
+			copia = numero;
+			for (i = 0; i < 2;) {
+				copia--;
+				if (copia == 1) {
+					printf("nao existe mais numeros a esquerda de %d\n", numero);
+					break;
+				}else if (ehPrimo(copia) == true) {
+					i++;
+					printf("O %d numero primo a esquerda de %d eh %d\n", i, numero, copia);
+				}	
 			}
-
+	
+			copia = numero;
+			for (i = 0; i < 2;) {
+				copia++;
+		
+				if (ehPrimo(copia) == true) {
+					i++;
+					printf("O %d numero primo a direita de %d eh %d\n", i, numero, copia);
+				}
+	
+			}
+		}else{
+			printf("nao eh primo\n");
 		}
-
 	}
-	else {
-		printf("nao eh primo\n");
-		goto INICIO;
-	}
-
 	return 0;
 }
 
-int ehPrimo(int numero) {
+bool ehPrimo(int numero) {
 
 	if (numero <= 1) {
-		return 0;
+		return false;
 	}
-
-	for (int i = 2; i <= numero / 2; i++) {
+	int i;
+	for ( i = 2; i <= numero / 2; i++) {
 		if (numero%i == 0) {
-			return 0;
+			return false;
 		}
 	}
 
-	return 1;
+	return true;
 
 }
