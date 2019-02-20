8861ebf179ea5041e2ad434372742b9d50618804
Joel <JoelVIctor1746@gmail.com>
2019-02-20 10:47:05 -0300

    Update 1435new.c

--- Lista02_03/1435new.c
+++ Lista02_03/1435new.c
@@ -6,13 +6,16 @@ void printarNumero(int *numero, int *coluna);
 int main(){
 
 	int n,posicao1, posicao2, fim, numero;
-	bool impar;
+	bool impar, key;
 	
 	n = 1;
 	while(n != 0){
 		scanf("%d", &n);
-	
+        
 		impar = false;	
+        key = false;
+        if(n == 1) key = true;
+        
 		if(n%2 == 1 ) {
 			n = n/2;
 			n++;
@@ -55,11 +58,15 @@ int main(){
 		            printarNumero(&matriz[posicao1][posicao2], &posicao2);    
 		        }
 		        for(posicao2 = n-1; posicao2 >= 0; posicao2--){
-		            printf(" %3d", matriz[posicao1][posicao2]);
+                    if(key){
+                        printf("%3d", matriz[posicao1][posicao2]);
+                    }else{
+                        printf(" %3d", matriz[posicao1][posicao2]);
+                    }
+		            
 		        }
 		        printf("\n");
-		    }
-			printf("\n");				
+		    }				
 		}else{
 			//print par
 			for(posicao1 = 0; posicao1 < n; posicao1++){
@@ -80,15 +87,15 @@ int main(){
 		        }
 		        printf("\n");
 		    }
-			printf("\n");	
-		}		
+		}	
+        printf("\n");		
 	}
 	
     return 0;
 }
 
 void printarNumero(int *numero, int *coluna){
-    if(coluna == 0){
+    if(*coluna == 0 ){
         printf("%3d", *numero);    
     }else{
         printf(" %3d", *numero);
