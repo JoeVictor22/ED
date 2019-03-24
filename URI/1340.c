4bca0a925d978699717a6baeb7b7d18440a8dce7
Joel <JoelVIctor1746@gmail.com>
2019-03-23 23:14:56 -0300

    Update 1340.c

--- URI/1340.c
+++ URI/1340.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 struct pilha{
@@ -6,59 +7,126 @@ struct pilha{
     int *elementos;
 };
 
-void create(struct pilha *p);
+struct fila{
+    int front;
+    int size;
+    int *elementos;
+};
+struct filaP{
+	int topo;
+	int *elementos;
+};
+
+//pilha
+void createPilha(struct pilha *p);
 int pop(struct pilha *p);
 void push(struct pilha *p, int elemento);
-bool isEmpty(struct pilha *p);
-void print(struct pilha *p);
-void liberar(struct pilha *p);
+bool isEmptyPilha(struct pilha *p);
+void printPilha(struct pilha *p);
+void liberarPilha(struct pilha *p);
+//FILA
+void createFila(struct fila *f);
+int popFront(struct fila *f);
+void add(struct fila *f, int elemento);
+bool isEmptyFila(struct fila *f);
+void printFila(struct fila *f);
+void liberarFila(struct fila *f);
+//PRIORIDADE
+void createFilaP(struct filaP *f);
+int popP(struct filaP *f);
+void pushP(struct filaP *f, int elemento);
+bool isEmptyFilaP(struct filaP *f);
+void printFilaP(struct filaP *f);
+void liberarFilaP(struct filaP *f);
 
 int main(){
 
+    struct fila f;
     struct pilha p;
-    struct pilha descartadas;
-    int i,n;
+    struct filaP fp;
+	int i, opc, x,n ;
+    bool pilha, fila, filaP;
 
-    scanf("%d", &n);
-    while(n != 0){
-		create(&p);
-	    create(&descartadas);
+	createPilha(&p);
+	createFila(&f);
+	createFilaP(&fp);
+	
+    while(scanf("%d", &n) != EOF){
+    	fila = true;
+    	pilha = true;
+    	filaP = true;
+        for(i = 0; i < n; i++){
+            scanf("%d", &opc);
+            scanf("%d", &x);
+            switch(opc){
+               
+                case 1:
+                    push(&p, x);
+                    add(&f, x);
+                    pushP(&fp, x);
+                    break;
+                case 2:
 
-        for(i = n; i >= 1;i--){
-
-		    push(&p, i);
+                    if(x != pop(&p)){
+                        pilha = false;
+                    }
+                    if(x != popFront(&f)){
+					    fila = false;
+                    }
+                    if(x != popP(&fp)){
+                        filaP = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+		
         }
-        while(p.topo > 0){
-            push(&descartadas, pop(&p));
-            pushToFront(&p, pop(&p));
+        
+        
+        if(pilha){
+            if(( fila || filaP)){
+                printf("not sure\n");
+            }else{
+                printf("stack\n");
+            }
+        }else if(fila){
+            if((filaP)){
+                printf("not sure\n");
+            }else{
+                printf("queue\n");
+            }
+        }else if(filaP){
+            printf("priority queue\n");
+        }else{
+            printf("impossible\n");
         }
-        printf("Discarded cards:");
-        print(&descartadas);
-		printf("Remaining card: %d\n", p.elementos[0]);
-		liberar(&p);
-		liberar(&p);
-    
-        scanf("%d", &n);
+        
+        liberarPilha(&p);
+        liberarFila(&f);
+        liberarFilaP(&fp);
+        
     }
 
+    
+	
     return 0;
 }
 
-
-void create(struct pilha *p){
+//PILHA
+void createPilha(struct pilha *p){
     p->topo = -1;
     p->elementos = (int*)malloc(1 * sizeof(int));
 }
 
 int pop(struct pilha *p){
-    if(isEmpty(p)){
-        printf("Está vazia\n");
+    if(isEmptyPilha(p)){
+        printf("Está vazia, retornando 0\n");
         return 0;
     }else{
         return p->elementos[p->topo--];
     }
 }
-
 void push(struct pilha *p, int elemento){
     p->topo++;
     int *aux = (int*)realloc(p->elementos, (p->topo+1) * sizeof(int));
@@ -70,38 +138,143 @@ void push(struct pilha *p, int elemento){
         p->elementos[p->topo] = elemento;
     }
 }
-void pushToFront(struct pilha *p, int elemento){
-    p->topo++;
-    int i,*aux = (int*)realloc(p->elementos, (p->topo+1) * sizeof(int));
-    if(aux == NULL){
-        printf("ERRO\n");
-        exit(0);
-    }else{
-        p->elementos = aux;
-        for(i = p->topo; i > 0; i--){
-            p->elementos[i] = p->elementos[i-1];    
-        }
-        p->elementos[0] = elemento;
-    }
-    
-}
-
-bool isEmpty(struct pilha *p){
+bool isEmptyPilha(struct pilha *p){
     if(p->topo == -1){
         return true;
     }else{
         return false;
     }
 }
-
-void print(struct pilha *p){
-    int i;
-    for(i = 0; i < p->topo; i++){
-        printf(" %d,", p->elementos[i]);
+void printPilha(struct pilha *p){
+   
+    if(isEmptyPilha(p)){
+        printf("Está vazia\n");
+    }else{
+        int i;
+	    for(i = 0; i < p->topo; i++){
+	        printf(" %d,", p->elementos[i]);
+	    }
+	    printf(" %d\n", p->elementos[p->topo]);
     }
-    printf(" %d\n", p->elementos[p->topo]);
+}
+void liberarPilha(struct pilha *p){
+	free(p->elementos);
+    p->topo = -1;
+    p->elementos = (int*)malloc(1 * sizeof(int));
 }
 
-void liberar(struct pilha *p){
-	free(p->elementos);
+
+//FILA
+void createFila(struct fila *f){
+    f->size = 0;
+    f->front= 0;
+    f->elementos = (int*)malloc(1 * sizeof(int));
 }
+int popFront(struct fila *f){
+    if(isEmptyFila(f)){
+        printf("Está vazia, retornando 0\n");
+        return 0;
+    }else{
+        return f->elementos[f->front++];
+    }
+
+}
+void add(struct fila *f, int elemento){
+    f->size += 1; 
+    int i,*aux = (int*)realloc(f->elementos, (f->size) * sizeof(int));
+    if(aux == NULL){
+        printf("ERRO\n");
+        exit(0);
+    }else{
+        f->elementos = aux; 
+        f->elementos[f->size-1] = elemento;
+    }
+        
+}
+
+bool isEmptyFila(struct fila *f){
+    if(f->front == f->size){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void printFila(struct fila *f){
+  
+    if(isEmptyFila(f)){
+        printf("Está vazia\n");
+    }else{
+        int i;
+   		for(i = f->front; i < f->size; i++){
+       		printf(" %d,", f->elementos[i]);
+    	}
+    }
+
+}
+void liberarFila(struct fila *f){
+	free(f->elementos);
+    f->size = 0;
+    f->front= 0;
+    f->elementos = (int*)malloc(1 * sizeof(int));
+}
+
+//PRIORIDADE
+void createFilaP(struct filaP *f){
+	f->topo = -1;
+	f->elementos = (int*)malloc(1* sizeof(int));
+}
+int popP(struct filaP *f){
+    if(isEmptyFilaP(f)){
+        printf("Está vazia, retornando 0\n");
+        return 0;
+    }else{
+        return f->elementos[f->topo--];
+    }
+}
+void pushP(struct filaP *f, int elemento){
+    f->topo++;
+    int i;
+    int *aux = (int*)realloc(f->elementos, (f->topo+1) * sizeof(int));
+    if(aux == NULL){
+        printf("ERRO\n");
+        exit(0);
+    }else{
+        f->elementos = aux;
+        i = f->topo-1;
+        while((i >= 0 )&& (f->elementos[i] > elemento )) {
+        	
+        	f->elementos[i+1] = f->elementos[i]; 
+        	i--;
+		}
+        f->elementos[i+1] = elemento;
+    }
+}
+
+bool isEmptyFilaP(struct filaP *f){
+    if(f->topo == -1){
+        return true;
+    }else{
+        return false;
+    }
+}
+void printFilaP(struct filaP *f){
+   
+    if(isEmptyFilaP(f)){
+        printf("Está vazia\n");
+    }else{
+        int i;
+	    for(i = 0; i < f->topo; i++){
+	        printf(" %d,", f->elementos[i]);
+	    }
+	    printf(" %d\n", f->elementos[f->topo]);
+    }
+    printf("\n");
+}
+void liberarFilaP(struct filaP *f){
+	free(f->elementos);
+    f->topo = -1;
+	f->elementos = (int*)malloc(1* sizeof(int));
+}
+
+
